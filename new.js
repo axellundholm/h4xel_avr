e32155b9c0ef92c3047558fbbe3243ed7d6058c2
Axel Lundholm <axel.lundholm@me.com>
2018-10-03 19:42:04 +0200

    new test-file

---
+++ new.js
@@ -0,0 +1,12 @@
+function test(testOut) {
+    console.log(testOut);
+    return testOut;
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
